46a074bb12336459d5cbcb82b1f715867ff18747
Luis Alfredo Campo Torres <lac2004t@gmail.com>
2025-05-30 18:54:44 -0500

    Actualizacion con los ultimos cambios

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('form');
+  
+  form.addEventListener('submit', function (event) {
+    alert('Gracias por tu mensaje, te responderé pronto.');
+  });
+});
